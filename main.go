542d195a2be09da88d408507b3443d757fa3e927
trunov <trunov@enterideas.com>
2022-02-11 16:54:15 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+)
+
+var scale_w = flag.Float64("scaleWidht", 1.2, "Scale of widht")
+var scale_h = flag.Float64("scaleHeight", 1.2, "Scale of height")
+
+// var scale_both = flag.Bool("scale both", true, "If true, scale w and h by w value")
+
+func main() {
+
+	// open output file
+
+	// make a write buffer
+	logrus.SetLevel(logrus.DebugLevel)
+	s := bufio.NewScanner(os.Stdin)
+	bufsize := 10 << 20
+	buf := make([]byte, bufsize)
+	s.Buffer(buf, bufsize)
+	for {
+		if s.Scan() {
+			reqdata := s.Bytes()
+			expand(reqdata)
+
+		}
+	}
+
+}
+
+func expand(reqdata []byte) {
+
+	items := gjson.ParseBytes(reqdata).Get("items")
+	if len(items.Array()) == 0 {
+		fmt.Println(string(reqdata))
+		return
+	}
+	itemsArray := []gjson.Result{}
+	itemsArray = append(itemsArray, items.Array()...)
+	responseStringArray := []string{}
+
+	// dets := [][]float64{}
+	for _, item := range itemsArray {
+		item_str := item.String()
+		bbox := []float64{}
+		item.Get("bbox").ForEach(func(key, value gjson.Result) bool {
+			bbox = append(bbox, value.Num)
+			return true
+		})
+
+
+		bbox_res := ResizeFromCenter(bbox, *scale_w, *scale_h)
+
+		item_str, _ = sjson.Set(item_str, "bbox.0", toFixed(bbox_res[0], 3))
+		item_str, _ = sjson.Set(item_str, "bbox.1", toFixed(bbox_res[1], 3))
+		item_str, _ = sjson.Set(item_str, "bbox.2", toFixed(bbox_res[2], 3))
+		item_str, _ = sjson.Set(item_str, "bbox.3", toFixed(bbox_res[3], 3))
+		responseStringArray = append(responseStringArray, item_str)
+	}
+
+	responseString := fmt.Sprintf("[%s]", strings.Join(responseStringArray, ", "))
+
+	reqdata_st, _ := sjson.SetRaw(string(reqdata), "items", responseString)
+	fmt.Println(string(reqdata_st))
+
+}
---
+++ utils.go
@@ -0,0 +1,35 @@
+package main
+
+import "math"
+
+//ResizeFromCenter resizes a bounding box by a scale factor from its center
+func ResizeFromCenter(bbox []float64, scale_w float64, scale_h float64) []float64 {
+
+	if scale_h == 0 || scale_w == 0 {
+		return bbox
+	}
+	w := bbox[2]
+	h := bbox[3]
+
+	dx := scale_w * w / 2
+	dy := scale_h * h / 2
+	xc := bbox[0] + w/2
+	yc := bbox[1] + h/2
+
+	bbox2 := make([]float64, 4)
+	bbox2[0] = math.Max(xc-dx, 0)
+	bbox2[1] = math.Max(yc-dy, 0)
+	bbox2[2] = math.Min(xc+dx-bbox2[0], 1)
+	bbox2[3] = math.Min(yc+dy-bbox2[1], 1)
+
+	return bbox2
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
